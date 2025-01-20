d44b6975cb862dee5cb85426218ce8a3c8f7d3fe
Ben Swift <ben@benswift.me>
2025-01-20 15:20:39 +1100

    add some tests for relationship votes

--- test/revelo/relationship_test.exs
+++ test/revelo/relationship_test.exs
@@ -5,6 +5,7 @@ defmodule Revelo.RelationshipTest do
   import ReveloTest.Generators
 
   alias Revelo.Diagrams.Relationship
+  alias Revelo.Diagrams.RelationshipVote
 
   describe "relationship actions" do
     property "succeeds on all valid create input" do
@@ -37,6 +38,55 @@ defmodule Revelo.RelationshipTest do
       assert relationship.session
       assert relationship.src
       assert relationship.dst
+    end
+
+    test "can create relationship vote" do
+      user = user()
+      session = session()
+      var1 = variable(user: user, session: session)
+      var2 = variable(user: user, session: session)
+      relationship = relationship(user: user, session: session, src: var1, dst: var2)
+
+      vote =
+        RelationshipVote
+        |> Ash.Changeset.for_create(:create, %{relationship: relationship}, actor: user)
+        |> Ash.create!()
+
+      assert vote.voter_id == user.id
+      assert vote.relationship_id == relationship.id
+    end
+
+    test "can create votes for multiple relationships with same user" do
+      user = user()
+      session = session()
+      var1 = variable(user: user, session: session)
+      var2 = variable(user: user, session: session)
+      var3 = variable(user: user, session: session)
+
+      relationship1 = relationship(user: user, session: session, src: var1, dst: var2)
+      relationship2 = relationship(user: user, session: session, src: var2, dst: var3)
+
+      vote1 =
+        RelationshipVote
+        |> Ash.Changeset.for_create(:create, %{relationship: relationship1}, actor: user)
+        |> Ash.create!()
+
+      vote2 =
+        RelationshipVote
+        |> Ash.Changeset.for_create(:create, %{relationship: relationship2}, actor: user)
+        |> Ash.create!()
+
+      assert vote1.voter_id == user.id
+      assert vote2.voter_id == user.id
+      assert vote1.relationship_id == relationship1.id
+      assert vote2.relationship_id == relationship2.id
+
+      relationship1 = Ash.load!(relationship1, :votes)
+      relationship2 = Ash.load!(relationship2, :votes)
+      relationship1_id = relationship1.id
+      relationship2_id = relationship2.id
+      assert [%{relationship_id: ^relationship1_id}] = relationship1.votes
+      assert [%{relationship_id: ^relationship2_id}] = relationship2.votes
     end
   end
 end
