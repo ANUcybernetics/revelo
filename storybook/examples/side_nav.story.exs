d7745d771bee20918c7bc87502328002973924ad
adrian-g-schmidt <adrian.g.schmidt@gmail.com>
2025-01-22 17:27:02 +1100

    Add SideNav to Storybook

---
+++ storybook/examples/side_nav.story.exs
@@ -0,0 +1,19 @@
+defmodule Storybook.Examples.SideNav do
+  @moduledoc false
+  use PhoenixStorybook.Story, :example
+
+  import ReveloWeb.UIComponents
+
+  def doc do
+    "The navigation menu for the facilitator"
+  end
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <div class="scale-[1] h-[500px]">
+      <.sidebar />
+    </div>
+    """
+  end
+end
