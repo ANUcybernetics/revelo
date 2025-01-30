a5f0bf1f61a272b27a944e449fc8c1239008a722
Ben Swift <ben@benswift.me>
2025-01-30 13:25:13 +0800

    add a test to log in a user
    includes some helpful "set up a logged in user" code

---
+++ test/revelo_web/controllers/login_test.exs
@@ -0,0 +1,24 @@
+defmodule ReveloWeb.LoginTest do
+  use ReveloWeb.ConnCase
+
+  import ReveloTest.Generators
+
+  def create_session_with_user(conn) do
+    password = "657]545asdflh"
+    user = user_with_password(password)
+
+    conn
+    |> visit("/sign-in/")
+    |> fill_in("#user-password-sign-in-with-password_email", "Email", with: Ash.CiString.value(user.email))
+    |> fill_in("#user-password-sign-in-with-password_password", "Password", with: password)
+    |> submit()
+  end
+
+  describe "real (non-anonymous) users can do stuff" do
+    test "can create a new session", %{conn: conn} do
+      conn
+      |> create_session_with_user()
+      |> assert_has("h1", text: "Phoenix Framework")
+    end
+  end
+end
