44ab6f995108f4c08bdc9a14137c47acdcf38aff
Ben Swift <ben@benswift.me>
2025-01-30 14:23:13 +0800

    update user flow test

--- test/revelo_web/controllers/user_flow_test.exs
+++ test/revelo_web/controllers/user_flow_test.exs
@@ -3,24 +3,35 @@ defmodule ReveloWeb.UserFlowTest do
 
   import ReveloTest.Generators
 
-  def create_session_with_user(conn) do
-    password = "657]545asdflh"
-    user = user_with_password(password)
-
+  def log_in_user(conn, email, password) do
     conn
     |> visit("/sign-in/")
-    |> fill_in("#user-password-sign-in-with-password_email", "Email", with: Ash.CiString.value(user.email))
+    |> fill_in("#user-password-sign-in-with-password_email", "Email", with: email)
     |> fill_in("#user-password-sign-in-with-password_password", "Password", with: password)
     |> submit()
   end
 
   describe "real (non-anonymous) users can do stuff" do
     test "can create a new session", %{conn: conn} do
+      password = "657]545asdflh"
+      user = user_with_password(password)
+
       conn
-      |> create_session_with_user()
+      |> log_in_user(Ash.CiString.value(user.email), password)
       |> assert_has("h1", text: "Revelo")
       |> click_link("Sessions")
       |> click_link("New Session")
+    end
+  end
+
+  describe "QR-code anon-user-creation" do
+    test "can log in", %{conn: conn} do
+      password = "657]545asdflh"
+      user = user_with_password(password)
+
+      static = log_in_user(conn, Ash.CiString.value(user.email), password)
+
+      assert static.conn.assigns.current_user.id == user.id
     end
   end
 end
